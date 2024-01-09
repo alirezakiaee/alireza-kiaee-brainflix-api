cc8f0d053405ffb4508598fa2e654bf20113776f
alirezakiaee <alirezakiaee91@gmail.com>
2024-01-09 00:00:23 -0500

    added videos.json and updated the videos.js with routes

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const PORT = 8080;
 const videos = require('./routes/videos');
 
 
-app.use('/videos', videos);
+app.use(express.json());
+app.use('/', videos);
+app.use('/:id', videos);
+
 
 
 app.listen(PORT, () => {
