dda871083817a24179702f30a1fdb37607218d0f
alirezakiaee <alirezakiaee91@gmail.com>
2024-01-09 20:37:57 -0500

    added the CORS to the server

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const videos = require('./routes/videos');
+const cors = require('cors');
 
-
+app.use(cors());
 app.use(express.json());
 app.use('/', videos);
 app.use('/:id', videos);
 
 
-
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
