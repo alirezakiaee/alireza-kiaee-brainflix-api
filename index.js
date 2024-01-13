7c4bab07914cadc09fa1c0cb1eadc0e013eecd5a
alirezakiaee <alirezakiaee91@gmail.com>
2024-01-13 09:11:34 -0500

    added CORS_ORIGIN to  the code

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const videos = require('./routes/videos');
 require ('dotenv').config();
-const { PORT,BACKEND } = process.env;
+const { PORT,CORS_ORIGIN } = process.env;
 // const PORT = process.env.PORT || 8080;
 
 const cors = require('cors');
 
 app.use(express.static('public'));
-app.use(cors( { origin: '*' } ));
+app.use(cors( { origin: CORS_ORIGIN } ));
 app.use(express.json());
 app.use('/', videos);
 app.use('/:id', videos);
